a3da2d4f0a82926b9e550189212587799e90a93a
root <root@room9pc01.tedu.cn>
2019-06-04 21:34:13 +0800

    rm logbak.sh

--- logbak.sh
+++
@@ -1,5 +0,0 @@
-date=$(date+%Y%m%d)   #年月日
-logpath=/usr/local/nginx/logs       
-mv $logpath/access.log $logpath/access-$date.log   #实时备份access.log
-mv $logpath/error.log $logpath/error-$date.log
-kill -USER1 $(cat $logpath/nginx.pid) #打开新的日志,没有则自动生成文件xxx.log  
