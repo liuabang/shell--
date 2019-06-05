af195a5c65c7b4d9044a5426c9bf9d03978ee03d
root <root@room9pc01.tedu.cn>
2019-06-05 21:50:18 +0800

    delete

--- logbak.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-#功能:该脚本能够自动化,且实时切割日志. 
-#作者:liusingbon
-date=$(date+%Y%m%d)   #年月日
-logpath=/usr/local/nginx/logs       
-mv $logpath/access.log $logpath/access-$date.log   #实时备份access.log
-mv $logpath/error.log $logpath/error-$date.log
-kill -USER1 $(cat $logpath/nginx.pid) #打开新的日志,没有则自动生成文件xxx.log 
